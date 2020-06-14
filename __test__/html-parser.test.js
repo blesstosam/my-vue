5080e4eaba91c67c0c087186e1826f04af7919f1
blesstosam <805902285@qq.com>
2020-06-14 18:02:54 +0800

    add jest and add html-parser

---
+++ __test__/html-parser.test.js
@@ -0,0 +1,40 @@
+const { parseToAST, parseText } = require('../compile/html-parser')
+
+test('test `parseToAST` params => <div class="wrap"><span>hello</span></div>', () => {
+  const html = '<div class="wrap"><span>hello</span></div>'
+
+  // ast = {
+  //   type: 1,
+  //   tag: 'div',
+  //   attrsList: [[]],
+  //   parent: undefined,
+  //   children: [
+  //     {
+  //       type: 1,
+  //       tag: 'span',
+  //       attrsList: [],
+  //       parent: {type: 1, tag: 'div', /*...*/},
+  //       children: [
+  //         {
+  //           type: 2,
+  //           text: 'hello'
+  //         }
+  //       ]
+  //     }
+  //   ],
+  // }
+
+  const ast = parseToAST(html)
+
+  expect(ast.tag).toBe('div')
+  expect(ast.children[0].tag).toBe('span')
+  expect(ast.children[0].children[0].text).toBe('hello')
+})
+
+test('test `parseText` params => 你好{{name}}', () => {
+  expect(parseText('你好{{name}}')).toBe('"你好"+_s(name)')
+})
+
+test('test `parseText` params => 你好{{name}}，已经{{age}}岁了', () => {
+  expect(parseText('你好{{name}}，已经{{age}}岁了')).toBe('"你好"+_s(name)+"，已经"+_s(age)+"岁了"')
+})
